38df54280dafe91c753697b94f553e1ce513ad74
KengoNagata <majorboy47@gmail.com>
2019-12-09 07:14:48 +0900

    Posts 一覧画面のひな形を作成
    補足
    ルーティングは仮置きで設定した

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <link href="/css/style.css" rel="stylesheet" type="text/css">
+  <title></title>
+</head>
+<body>
+  <div class="container">
+    <h1 class="head">英語の名言</h1>
+    <p class="content">When you come to a roadblock, take a detour.</p>
+    <p class="source">メアリー・ケイ・アッシュ</p>
+    <p class="content"></p>
+    <p class="source"></p>
+    <p class="content"></p>
+    <p class="source"></p>
+    <p class="content"></p>
+    <p class="source"></p>
+  </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,16 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
+// Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::resource('posts', 'App\Controllers\PostsController');
+
+Route::get('/', 'PostsController@index');
