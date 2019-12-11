91ab557b520e17c71065f1f22720a1c7b651907b
KengoNagata <majorboy47@gmail.com>
2019-12-12 07:10:31 +0900

    Post一覧画面 重複コードを削除し共通化

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,123 +1,106 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="utf-8">
-  <title></title>
-  <link href="/css/app.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-  <header class="header">
-    <div class="header__bar">
-      <h1 class="title"><a href="/">Quotes</a></h1>
-      <div class="nav">
-        <a href="{{ route('login') }}">ログイン</a>
-        @if (Route::has('register'))
-          <a href="{{ route('register') }}">新規登録</a>
-        @endif
-      </div>
-    </div>
-  </header>
-  <div class="container">
-    @foreach($posts as $post)
-    <div class="card quote">
-      <div class="content">
-        <img class="" alt="noimage", src="/storage/no_image.png" width="50" height="50">
-        {{ $post->body }}
-        @isset($post->translation)
-          <div class="translation">
-            {{ $post->translation }}
-          </div>
-        @endisset
-        <div class="source">
-          {{ $post->source }}
-        </div>
-      </div>
-      <div class="card-footer">
-        <div class="user">
-          マルクス
-        </div>
-        <div class="comment">
-          <a href="{{ action('PostsController@show', $post)}}">コメント</a>
-        </div>
-      </div>
-    </div>
-    @endforeach
-    <div class="card quote">
-      <div class="content">
-        <img class="" alt="noimage", src="/storage/no_image.png" width="50" height="50">
-        Be quick, but don’t hurry.
-        <div class="translation">
-          機敏であれ、しかし慌ててはいけない。
-        </div>
-        <div class="source">
-        ジョン・ウッデン
-        </div>
-      </div>
-      <div class="card-footer">
-        マルクス
-      </div>
-    </div>
-    <div class="card quote">
-      <div class="content">
-        <img class="" alt="noimage", src="/storage/no_image.png" width="50" height="50">
-        Be quick, but don’t hurry.
-        <div class="translation">
-          機敏であれ、しかし慌ててはいけない。
-        </div>
-        <div class="source">
-        ジョン・ウッデン
-        </div>
-      </div>
-      <div class="card-footer">
-        マルクス
-      </div>
-    </div>
-    <div class="card quote">
-      <div class="content">
-        <img class="" alt="noimage", src="/storage/no_image.png" width="50" height="50">
-        Be quick, but don’t hurry.
-        <div class="translation">
-          機敏であれ、しかし慌ててはいけない。
-        </div>
-        <div class="source">
-        ジョン・ウッデン
-        </div>
-      </div>
-      <div class="card-footer">
-        マルクス
-      </div>
-    </div>
-    <div class="card quote">
-      <div class="content">
-        <img class="" alt="noimage", src="/storage/no_image.png" width="50" height="50">
-        Be quick, but don’t hurry.
-        <div class="translation">
-          機敏であれ、しかし慌ててはいけない。
-        </div>
-        <div class="source">
-        ジョン・ウッデン
-        </div>
-      </div>
-      <div class="card-footer">
-        マルクス
-      </div>
-    </div>
-    <div class="card quote">
-      <div class="content">
-        <img class="" alt="noimage", src="/storage/no_image.png" width="50" height="50">
-        Be quick, but don’t hurry.
-        <div class="translation">
-          機敏であれ、しかし慌ててはいけない。
-        </div>
-        <div class="source">
-        ジョン・ウッデン
-        </div>
-      </div>
-      <div class="card-footer">
-        マルクス
-      </div>
-    </div>
+@extends('layouts.default')
 
+@section('title', 'Quotes')
+
+@section('content')
+  @foreach($posts as $post)
+  <div class="card quote">
+    <div class="content">
+      <img class="" alt="noimage", src="/storage/no_image.png" width="50" height="50">
+      {{ $post->body }}
+      @isset($post->translation)
+        <div class="translation">
+          {{ $post->translation }}
+        </div>
+      @endisset
+      <div class="source">
+        {{ $post->source }}
+      </div>
+    </div>
+    <div class="card-footer">
+      <div class="user">
+        マルクス
+      </div>
+      <div class="comment">
+        <a href="{{ action('PostsController@show', $post)}}">コメント</a>
+      </div>
+    </div>
   </div>
-</body>
-</html>
+  @endforeach
+  <div class="card quote">
+    <div class="content">
+      <img class="" alt="noimage", src="/storage/no_image.png" width="50" height="50">
+      Be quick, but don’t hurry.
+      <div class="translation">
+        機敏であれ、しかし慌ててはいけない。
+      </div>
+      <div class="source">
+      ジョン・ウッデン
+      </div>
+    </div>
+    <div class="card-footer">
+      マルクス
+    </div>
+  </div>
+  <div class="card quote">
+    <div class="content">
+      <img class="" alt="noimage", src="/storage/no_image.png" width="50" height="50">
+      Be quick, but don’t hurry.
+      <div class="translation">
+        機敏であれ、しかし慌ててはいけない。
+      </div>
+      <div class="source">
+      ジョン・ウッデン
+      </div>
+    </div>
+    <div class="card-footer">
+      マルクス
+    </div>
+  </div>
+  <div class="card quote">
+    <div class="content">
+      <img class="" alt="noimage", src="/storage/no_image.png" width="50" height="50">
+      Be quick, but don’t hurry.
+      <div class="translation">
+        機敏であれ、しかし慌ててはいけない。
+      </div>
+      <div class="source">
+      ジョン・ウッデン
+      </div>
+    </div>
+    <div class="card-footer">
+      マルクス
+    </div>
+  </div>
+  <div class="card quote">
+    <div class="content">
+      <img class="" alt="noimage", src="/storage/no_image.png" width="50" height="50">
+      Be quick, but don’t hurry.
+      <div class="translation">
+        機敏であれ、しかし慌ててはいけない。
+      </div>
+      <div class="source">
+      ジョン・ウッデン
+      </div>
+    </div>
+    <div class="card-footer">
+      マルクス
+    </div>
+  </div>
+  <div class="card quote">
+    <div class="content">
+      <img class="" alt="noimage", src="/storage/no_image.png" width="50" height="50">
+      Be quick, but don’t hurry.
+      <div class="translation">
+        機敏であれ、しかし慌ててはいけない。
+      </div>
+      <div class="source">
+      ジョン・ウッデン
+      </div>
+    </div>
+    <div class="card-footer">
+      マルクス
+    </div>
+  </div>
+@endsection
+
