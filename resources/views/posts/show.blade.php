76bef5eb152ee5a7278e2dd1cfdf3292ee5e579e
KengoNagata <majorboy47@gmail.com>
2019-12-12 07:12:23 +0900

    Post詳細画面  重複コードを削除し共通化

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,40 +1,23 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="utf-8">
-  <title></title>
-  <link href="/css/app.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-  <header class="header">
-    <div class="header__bar">
-      <h1 class="title"><a href="/">Quotes</a></h1>
-      <div class="nav">
-        <a href="{{ route('login') }}">ログイン</a>
-        @if (Route::has('register'))
-          <a href="{{ route('register') }}">新規登録</a>
-        @endif
+@extends('layouts.default')
+
+@section('title', 'Quotes')
+
+@section('content')
+  <div class="card quote">
+    <div class="content">
+      <img class="" alt="noimage", src="/storage/no_image.png" width="50" height="50">
+      {!! nl2br(e($post->body)) !!}
+      @isset($post->translation)
+        <div class="translation">
+          {!! nl2br(e($post->translation)) !!}
+        </div>
+      @endisset
+      <div class="source">
+        {{ $post->source }}
       </div>
     </div>
-  </header>
-  <div class="container">
-    <div class="card quote">
-      <div class="content">
-        <img class="" alt="noimage", src="/storage/no_image.png" width="50" height="50">
-        {!! nl2br(e($post->body)) !!}
-        @isset($post->translation)
-          <div class="translation">
-            {!! nl2br(e($post->translation)) !!}
-          </div>
-        @endisset
-        <div class="source">
-          {{ $post->source }}
-        </div>
-      </div>
-      <div class="card-footer">
-        マルクス
-      </div>
+    <div class="card-footer">
+      マルクス
     </div>
   </div>
-</body>
-</html>
+@endsection
