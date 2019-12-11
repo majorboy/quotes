4a80c071182d0862dc6f9b2b933a34ecb76b671c
KengoNagata <majorboy47@gmail.com>
2019-12-12 07:05:07 +0900

    デフォルト画面のひな形を作成
    目的
    index / show アクションのコード重複部分を共通化する準備の為

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>@yield('title')</title>
+  <link href="/css/app.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+  <header class="header">
+    <div class="header__bar">
+      <h1 class="title"><a href="/">Quotes</a></h1>
+      <div class="nav">
+        <a href="{{ route('login') }}">ログイン</a>
+        @if (Route::has('register'))
+          <a href="{{ route('register') }}">新規登録</a>
+        @endif
+      </div>
+    </div>
+  </header>
+  <div class="container">
+    @yield('content')
+  </div>
+</body>
+</html>
