45386a8723086f12e65602f63c9dac760406086d
KengoNagata <majorboy47@gmail.com>
2019-12-12 06:58:13 +0900

    Post詳細画面→改行表示できるように設定

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -21,10 +21,10 @@
     <div class="card quote">
       <div class="content">
         <img class="" alt="noimage", src="/storage/no_image.png" width="50" height="50">
-        {{ $post->body }}
+        {!! nl2br(e($post->body)) !!}
         @isset($post->translation)
           <div class="translation">
-            {{ $post->translation }}
+            {!! nl2br(e($post->translation)) !!}
           </div>
         @endisset
         <div class="source">
