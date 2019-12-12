4e711e1076839019522acea36210e16ef525cbc4
KengoNagata <majorboy47@gmail.com>
2019-12-13 06:53:47 +0900

    Post/Userモデル　リレーションを定義

--- app/Post.php
+++ app/Post.php
@@ -4,7 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Post extends Model
 {
-    protected $fillable = ['source', 'body', 'user_id'];
+    protected $fillable = ['source', 'body', 'translation', 'user_id'];
+
+    public function user() 
+    {
+        return $this->belongsTo('App\User');
+    }
+
 }
