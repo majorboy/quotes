babbe6fd753f92ae5f272f07c6df73f923a47590
KengoNagata <majorboy47@gmail.com>
2019-12-15 07:21:36 +0900

    PostReQuestにバリデーションを定義
    補足
    エラーメッセージも各々日本語で追加した。

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -13,7 +13,7 @@ class PostRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,20 @@ class PostRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'body' => 'required|max:500',
+            'translation'=> 'max:500',
+            'source' => 'required|max:150'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'body.required' => 'Quoteを入力してください',
+            'body.max:500' => 'Quoteは500文字以内で入力してください',
+            'translation.max:500' => '日本語訳は500文字以内で入力してください',
+            'source.required' => '出典を入力してください',
+            'source.max:150' => '出典は150文字以内で入力してください',
         ];
     }
 }
