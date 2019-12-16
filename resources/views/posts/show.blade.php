4bda10556b44bd4fa8e5b8d7fe8c858989f2739d
KengoNagata <majorboy47@gmail.com>
2019-12-17 05:28:30 +0900

    Comment表示画面のモックアップを作成

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -22,4 +22,84 @@
       </div>
     </div>
 </div>
+<div class="comment-container">
+  <ul class="comments list-unstyled">
+    <li class="clearfix comment media">
+      <img class="" alt="noimage" class="comment__icon align-self-start"  src="/storage/no_image.png" width="70" height="70">
+      <div class="media-body">
+        <h5 class="comment__user_name">marx</h5>
+        <div class="comment__content">
+          <div class="comment__content__text">
+          誰かのことを批判したくなったときには、
+          世間のすべての人がおまえのように恵まれているわけではないということをちょっと思いだしてみるのだ。
+
+          Whenever you feel like criticizing anyone, 
+          just remember that all the people in this world haven’t had the advantages that you’ve had.
+          </div>
+        </div>
+      </div>
+    </li>
+    <li class="clearfix comment media">
+      <img class="" alt="noimage" class="comment__icon align-self-start"  src="/storage/no_image.png" width="70" height="70">
+      <div class="media-body">
+        <h5 class="comment__user_name">marx</h5>
+        <div class="comment__content">
+          <div class="comment__content__text">
+          誰かのことを批判したくなったときには、
+          世間のすべての人がおまえのように恵まれているわけではないということをちょっと思いだしてみるのだ。
+
+          Whenever you feel like criticizing anyone, 
+          just remember that all the people in this world haven’t had the advantages that you’ve had.
+          </div>
+        </div>
+      </div>
+    </li>
+    <li class="clearfix comment media">
+      <img class="" alt="noimage" class="comment__icon align-self-start"  src="/storage/no_image.png" width="70" height="70">
+      <div class="media-body">
+        <h5 class="comment__user_name">marx</h5>
+        <div class="comment__content">
+          <div class="comment__content__text">
+          誰かのことを批判したくなったときには、
+          世間のすべての人がおまえのように恵まれているわけではないということをちょっと思いだしてみるのだ。
+
+          Whenever you feel like criticizing anyone, 
+          just remember that all the people in this world haven’t had the advantages that you’ve had.
+          </div>
+        </div>
+      </div>
+    </li>
+    <li class="clearfix comment media">
+      <img class="" alt="noimage" class="comment__icon align-self-start"  src="/storage/no_image.png" width="70" height="70">
+      <div class="media-body">
+        <h5 class="comment__user_name">marx</h5>
+        <div class="comment__content">
+          <div class="comment__content__text">
+          誰かのことを批判したくなったときには、
+          世間のすべての人がおまえのように恵まれているわけではないということをちょっと思いだしてみるのだ。
+
+          Whenever you feel like criticizing anyone, 
+          just remember that all the people in this world haven’t had the advantages that you’ve had.
+          </div>
+        </div>
+      </div>
+    </li>
+    <li class="clearfix comment media">
+      <img class="" alt="noimage" class="comment__icon align-self-start"  src="/storage/no_image.png" width="70" height="70">
+      <div class="media-body">
+        <h5 class="comment__user_name">marx</h5>
+        <div class="comment__content">
+          <div class="comment__content__text">
+          誰かのことを批判したくなったときには、
+          世間のすべての人がおまえのように恵まれているわけではないということをちょっと思いだしてみるのだ。
+
+          Whenever you feel like criticizing anyone, 
+          just remember that all the people in this world haven’t had the advantages that you’ve had.
+          </div>
+        </div>
+      </div>
+    </li>
+
+  </ul>
+</div>
 @endsection
