8b4fda71ad5f5bfbcd0777af2345ae692c127c34
KengoNagata <majorboy47@gmail.com>
2019-12-17 21:29:53 +0900

    UsersController mypage表示のためのshowアクションを定義

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UsersController extends Controller
 {
-    //
+    public function show(User $user){
+        return view('users.show')->with('user', $user);
+    }
 }
