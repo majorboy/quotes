6a0c77849371bda41ca43b39252ecab7383f6a77
KengoNagata <majorboy47@gmail.com>
2019-12-17 20:14:50 +0900

    CommentRequestクラスを追加
    目的
    コメント投稿時のエラーメッセージ表示機能を実装する為
    
    補足
    文字数制限のエラーメッセージが読み込まれない不具合が出ていた為、併せて修正した。

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -34,10 +34,10 @@ class PostRequest extends FormRequest
     {
         return [
             'body.required' => 'Quoteを入力してください',
-            'body.max:500' => 'Quoteは500文字以内で入力してください',
-            'translation.max:500' => '日本語訳は500文字以内で入力してください',
+            'body.max' => 'Quoteは500文字以内で入力してください',
+            'translation.max' => '日本語訳は500文字以内で入力してください',
             'source.required' => '出典を入力してください',
-            'source.max:150' => '出典は150文字以内で入力してください',
+            'source.max' => '出典は150文字以内で入力してください',
         ];
     }
 }
