bad545ee53dc340e6320d510ac06dd907737f185
KengoNagata <majorboy47@gmail.com>
2019-12-18 22:08:16 +0900

    Post一覧画面の修正

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -28,17 +28,32 @@
       </div>
       <div class="comment">
         <a href="{{ action('PostsController@show', $post)}}">コメント</a>
-        @can('update',$post)
-          <a href="{{ action('PostsController@edit', $post)}}">編集</a>
-        @endcan
-        @can('delete',$post)
-          <a href="#" data-id="{{$post->id}}" class="delete">削除</a>
-        @endcan
-        <form method="post" action="{{ url('/posts', $post->id)}}" id="form_{{ $post->id }}"> 
+        <span class="count">{{ $post->comments()->count()}}</span>
+      </div>
+      @if(Auth::check() && Auth::user()->is_stock($post->id))
+        <a href="#" data-id="{{$post->id}}" class="unstock">ストック解除</a>
+        <span class="count">{{ $post->stock_users()->count()}}</span>
+        <form method="post" action="{{ route('stocks.unstock', $post->id) }}" id="unstock_{{ $post->id }}"> 
           {{ csrf_field() }}
           {{ method_field('delete') }}
         </form>
-      </div>
+      @else
+        <a href="#" data-id="{{$post->id}}" class="stock">ストックする</a>
+        <span class="count">{{ $post->stock_users()->count() }}</span>
+        <form method="post" action="{{ route('stocks.stock', $post->id) }}" id="stock_{{ $post->id }}">
+          {{ csrf_field() }}
+        </form>
+      @endif
+      @can('update',$post)
+        <a href="{{ action('PostsController@edit', $post)}}">編集</a>
+      @endcan
+      @can('delete',$post)
+        <a href="#" data-id="{{$post->id}}" class="delete">削除</a>
+      @endcan
+      <form method="post" action="{{ url('/posts', $post->id)}}" id="form_{{ $post->id }}"> 
+        {{ csrf_field() }}
+        {{ method_field('delete') }}
+      </form>
     </div>
   </div>
   @endforeach
