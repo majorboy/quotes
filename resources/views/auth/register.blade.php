ddba066e121eee15b1aa32a50ce08aa5f34e2e0e
KengoNagata <majorboy47@gmail.com>
2019-12-21 19:23:15 +0900

    ユーザー登録→アバター uploadフォームを追加

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -59,6 +59,13 @@
                             <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                         </div>
                     </div>
+                    <div class="form-group row">
+                        <label for="avatar" class="col-md-4 col-form-label text-md-right">{{ __('アバター (任意)') }}</label>
+
+                        <div class="col-md-6">
+                             <input type="file" class="form-control avatar" name="avatar" id="avatar">
+                        </div>
+                    </div>
 
                     <div class="form-group row mb-0">
                         <div class="col-md-6 offset-md-4">
