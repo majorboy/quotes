b7e0bff8b6d6f096136cc2bc5f9488a8eba36417
KengoNagata <majorboy47@gmail.com>
2019-12-22 17:25:09 +0900

    各Viewにアバター画像を追加
    内容
    ・ユーザーがアバター画像を登録している場合はアバター画像を表示、登録していない場合はデフォルトのユーザー画像を表示する

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8">
   <title>@yield('title')</title>
   <link href="/css/app.css" rel="stylesheet" type="text/css">
+  <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 <body>
   <header class="header">
@@ -11,13 +12,18 @@
       <h1 class="title"><a href="/">Quotes</a></h1>
       <div class="nav">
       @guest
-        <a href="{{ route('login') }}">ログイン</a>
-        <a href="{{ route('register') }}">新規登録</a>
+        <a href="{{ route('login') }}" class="nav__item">ログイン</a>
+        <a href="{{ route('register') }}" class="nav__item">新規登録</a>
       @else
-        <a href="{{action('StocksController@index', auth()->id())}}">ストック一覧</a>
-        <a href="{{ url('/users', auth()->id()) }}">マイページ</a>
-        <a href="{{ url('/posts/create') }}">投稿する</a>
-        <a href="{{ route('logout') }}">ログアウト</a>
+        <a href="{{ url('/posts/create') }}" class="nav__item">投稿する</a>
+        <a href="{{ route('logout') }}" class="nav__item">ログアウト</a>
+        @if(Auth::user()->has_avatar())
+          <img src="{{Auth::user()->getFirstMediaUrl('avatars', 'thumb') }}" class="nav__icon" width="35" height="35">
+        @else
+          <img src="/storage/default-user.png" alt="noimage" class="nav__icon" width="35" height="35">
+        @endif
+        <!-- <a href="{{action('StocksController@index', auth()->id())}}">ストック一覧</a>
+        <a href="{{ url('/users', auth()->id()) }}">マイページ</a> -->
       @endguest
       </div>
     </div>
