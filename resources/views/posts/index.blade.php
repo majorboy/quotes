1572678499e18ae29748563c6c0c42dee71712ea
KengoNagata <majorboy47@gmail.com>
2019-12-29 14:14:32 +0900

    デフォルトのユーザー画像→S3経由での呼び出しに変更

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -14,7 +14,7 @@
       @if($post->user->has_avatar())
         <img src="{{$post->user->getFirstMediaUrl('avatars', 'thumb') }}"width="50" height="50">
       @else
-        <img src="/storage/default-user.png" alt="noimage" width="50" height="50">
+        <img src="https://quotesbucket.s3-ap-northeast-1.amazonaws.com/1/default-user.png" alt="noimage" width="50" height="50">
       @endif
       <div class="body">{{ $post->body }}</div>
     </div>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,7 +8,7 @@
       @if($post->user->has_avatar())
         <img src="{{$post->user->getFirstMediaUrl('avatars', 'thumb') }}" width="50" height="50">
       @else
-        <img src="/storage/default-user.png" alt="noimage" width="50" height="50">
+        <img src="https://quotesbucket.s3-ap-northeast-1.amazonaws.com/1/default-user.png" alt="noimage" width="50" height="50">
       @endif
       <div class="body">{{ $post->body }}</div>
     </div>
@@ -33,7 +33,7 @@
         @if($comment->user->has_avatar())
           <img src="{{$comment->user->getFirstMediaUrl('avatars', 'thumb')}}" width="50" height="50">
         @else
-          <img src="/storage/default-user.png" alt="noimage" width="50" height="50">
+          <img src="https://quotesbucket.s3-ap-northeast-1.amazonaws.com/1/default-user.png" alt="noimage" width="50" height="50">
         @endif
         <div class="media-body">
           <div class="comment__head">
