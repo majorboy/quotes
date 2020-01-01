76a4221ec6eedf081ad182febb756639594d3019
KengoNagata <majorboy47@gmail.com>
2020-01-01 21:37:50 +0900

    Updateアクションを定義
    補足
    editのローカル変数の受け渡しも
    一部修正した

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Auth;
+
 
 class UsersController extends Controller
 {
@@ -15,8 +17,20 @@ class UsersController extends Controller
         ]);
     }
 
-    public function edit(User $user){
-        return view('users.edit');
+    public function edit(){
+        $user = \Auth::user();
+        return view('users.edit')->with('user',$user);
+    }
+
+    public function update(Request $request){
+        $user = \Auth::user();
+        $form = $request->all();
+
+        unset($form['_token']);
+
+        $user->fill($form)->save();
+
+        return back()->with('flash_message', 'プロフィールを更新しました');
     }
 
     public function pass(){
