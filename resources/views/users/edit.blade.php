edb7950fae1f6cbe0e9091c7bcb3c54a70c74f2f
KengoNagata <majorboy47@gmail.com>
2020-01-01 21:42:20 +0900

    UpdateアクションのViewを設定
    補足
    ・oldヘルパーで既存データを呼び出す用に設定した
    
    ・Update後のフラッシュメッセージも追加した

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -4,31 +4,36 @@
 <div class="row justify-content-center">
     <div class="col-md-10">
         <div class="card auth">
-            <div class="card-header">{{ __('プロフィール編集') }}</div>
+          @if (session('flash_message'))
+            <div class="alert alert-success">
+              {{ session('flash_message') }}
+            </div>
+          @endif
+          <div class="card-header">{{ __('プロフィール編集') }}</div>
 
-            <div class="card-body">
-                <form method="POST" action="{{ route('register') }}"enctype="multipart/form-data">
-                    @csrf
+          <div class="card-body">
+              <form method="post" action="{{ url('/users', $user->id) }}" enctype="multipart/form-data">
+                  {{ csrf_field() }}
+                  {{ method_field('patch') }}
+                  <div class="form-group row">
+                      <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('名前') }}</label>
 
-                    <div class="form-group row">
-                        <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('名前') }}</label>
+                      <div class="col-md-6">
+                          <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name', $user->name) }}" required autocomplete="name" autofocus>
 
-                        <div class="col-md-6">
-                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                            @error('name')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                    </div>
+                          @error('name')
+                              <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $message }}</strong>
+                              </span>
+                          @enderror
+                      </div>
+                  </div>
 
                     <div class="form-group row">
                         <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
 
                         <div class="col-md-6">
-                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email',$user->email) }}" required autocomplete="email">
 
                             @error('email')
                                 <span class="invalid-feedback" role="alert">
