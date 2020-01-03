10eaee00062170ae24a0c9937a760ad945241eed
KengoNagata <majorboy47@gmail.com>
2020-01-03 15:35:24 +0900

    パスワード変更フォームを作成
    補足
    ・パスワード変更の成功/_失敗に応じた
    フラッシュメッセージ表示機能も併せて実装した

--- resources/views/passwords/edit.blade.php
+++ resources/views/passwords/edit.blade.php
@@ -5,15 +5,24 @@
     <div class="col-md-10">
         <div class="card auth">
             <div class="card-header">{{ __('パスワード変更') }}</div>
-
+            @if (session('change_password_success'))
+              <div class="alert alert-success">
+                {{ session('change_password_success') }}
+              </div>
+            @endif
+            @if (session('change_password_error'))
+              <div class="alert alert-danger">
+                {{ session('change_password_error') }}
+              </div>
+            @endif
             <div class="card-body">
-                <form method="POST" action="{{ route('register') }}"enctype="multipart/form-data">
-                    @csrf
+                <form method="POST" action="{{route('pass')}}">
+                    {{ csrf_field() }}
                     <div class="form-group row">
                         <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('現在のパスワード') }}</label>
 
                         <div class="col-md-6">
-                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="current-password" required autocomplete="new-password">
 
                             @error('password')
                                 <span class="invalid-feedback" role="alert">
@@ -27,7 +36,7 @@
                         <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('新パスワード') }}</label>
 
                         <div class="col-md-6">
-                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="new-password" required autocomplete="new-password">
 
                             @error('password')
                                 <span class="invalid-feedback" role="alert">
@@ -41,7 +50,7 @@
                         <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('新パスワード（確認）') }}</label>
 
                         <div class="col-md-6">
-                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            <input id="password-confirm" type="password" class="form-control" name="new-password_confirmation" required autocomplete="new-password">
                         </div>
                     </div>
                     <div class="form-group row mb-0">
